957ada3d9e71b97a79b0096fdd74f8481b04154b
Amal <amalprakash2025@gmail.com>
2022-12-07 00:34:21 +0530

    Added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,41 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddCourseComponent } from './add-course/add-course.component';
+import { RouterModule, Routes } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ViewCourseComponent } from './view-course/view-course.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+const myRoute:Routes = [
+  {
+    path:"",
+    component:AddCourseComponent
+  },
+  {
+    path:"view",
+    component:ViewCourseComponent
+  },
+  {
+    path:"navbar",
+    component:NavbarComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddCourseComponent,
+    NavbarComponent,
+    ViewCourseComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoute),
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
